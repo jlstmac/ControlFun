cd868132364bc18c1106bea4c25afa63a585aa45
jlstmac <jlstmac@gmail.com>
2016-01-16 11:40:32 +0800

    first commit

---
+++ ControlFun/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ControlFun
+//
+//  Created by LinShan Jiang on 16/1/16.
+//  Copyright © 2016年 LinShan Jiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ControlFun/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ControlFun
+//
+//  Created by LinShan Jiang on 16/1/16.
+//  Copyright © 2016年 LinShan Jiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
